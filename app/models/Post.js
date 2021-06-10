e0d09eb653a0be76a8347b94041f65e35e90b653
Vishal Nirmal <nirmalvishal24@gmail.com>
2021-06-10 17:14:33 +0530

    updated model to include creatorId

--- app/models/Post.js
+++ app/models/Post.js
@@ -4,6 +4,7 @@ const postSchema = mongoose.Schema({
     title: String,
     message: String,
     creator: String,
+    creatorId: mongoose.Schema.Types.ObjectId,
     tags: [String],
     selectedFile: String,
     likeCount: {
