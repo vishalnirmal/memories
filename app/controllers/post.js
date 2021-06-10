20518276b2beebbbc9a0e55030afda3e102da13a
Vishal Nirmal <nirmalvishal24@gmail.com>
2021-06-10 17:11:57 +0530

    Sending posts in descending order of their creation time

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -2,7 +2,7 @@ import Post from '../models/Post.js';
 
 export const getPosts = async (req, res) => {
     try {
-        const posts = await Post.find({});
+        const posts = await Post.find({}).sort({createdAt: -1});
         res.status(200).json(posts);
     } catch (error) {
         res.status(404).json({
