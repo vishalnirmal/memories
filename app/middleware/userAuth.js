d92c53bfaa48ace71e4e019545906c296b456d3d
Vishal Nirmal <nirmalvishal24@gmail.com>
2021-06-11 21:35:35 +0530

    verifying the user id to validate the token

--- app/middleware/userAuth.js
+++ app/middleware/userAuth.js
@@ -1,5 +1,6 @@
 import {config} from 'dotenv';
 import jwt from 'jsonwebtoken';
+import User from '../models/User.js';
 config();
 
 const auth = async (req, res, next) => {
@@ -10,6 +11,12 @@ const auth = async (req, res, next) => {
         });
     try {
         const data = await jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(data._id);
+        if (!user){
+            return res.status(401).json({
+                message: "Invalid Token"
+            });
+        }
         req.userId = data._id;
         next();
     } catch (error) {
