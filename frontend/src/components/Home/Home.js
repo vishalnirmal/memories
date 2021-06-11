4351a11181c28b9410abe1aedd0dc3a27d427638
Vishal Nirmal <nirmalvishal24@gmail.com>
2021-06-11 17:18:25 +0530

    now fetching posts when home component is rendered

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
+import {getPosts} from '../../redux/actions/post';
 import './Home.scss';
-import { useSelector } from 'react-redux';
 
 function Home() {
+    const dispatch = useDispatch();
     const {token} = useSelector(state => state.token);
+    useEffect(()=>{
+        dispatch(getPosts());
+    });
     return (
         <div className="container">
                 <Posts/>
